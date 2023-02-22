7b9157893b36f51348ca6e0edc70cd504aca9163
Morgan1420 <jan.moran.r@gmail.com>
2023-02-22 18:13:56 +0100

    making buttons

--- game_hub/lib/main.dart
+++ game_hub/lib/main.dart
@@ -9,7 +9,7 @@ class MainMenu extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Main Menu',
+      title: 'Game HUB',
       theme: ThemeData(primarySwatch: Colors.blue),
       home: MainMenu_sf(),
     );
@@ -24,6 +24,31 @@ class MainMenu_sf extends StatefulWidget {
 class _MainMenuState extends State<MainMenu_sf> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(title: const Text('Main Menu')),
+      body: Column(children: <Widget>[
+        Container(
+            alignment: Alignment.center,
+            child: Column(
+              children: <Widget>[
+                buildButton("buttonText", Colors.amber, 1),
+                buildButton("Minesweeper", Colors.amber, 1),
+              ],
+            )),
+      ]),
+    );
+  }
+
+  Widget buildButton(
+      String buttonText, Color buttonColor, double buttonHeight) {
+    return Container(
+      height: MediaQuery.of(context).size.height * .1 * buttonHeight,
+      width: MediaQuery.of(context).size.width * 0.8,
+      color: buttonColor,
+      child: TextButton(
+        onPressed: null,
+        child: Text(buttonText),
+      ),
+    );
   }
 }
