5b37c5f2da86dd45d3b56c9027606a6ca106cebc
Morgan1420 <jan.moran.r@gmail.com>
2023-02-23 19:22:22 +0100

    minesweeper widget created

---
+++ game_hub/lib/minesweeper.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Minesweeper extends StatefulWidget {
+  @override
+  MinesweeperState createState() => MinesweeperState();
+}
+
+class MinesweeperState extends State<Minesweeper> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
