d90a52064cfe097461ef84d84e84b8d3a0be4991
Morgan1420 <jan.moran.r@gmail.com>
2023-02-25 01:21:18 +0100

    solution matrix generates correctly

--- game_hub/lib/minesweeper.dart
+++ game_hub/lib/minesweeper.dart
@@ -1,3 +1,4 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 
 class Minesweeper extends StatefulWidget {
@@ -6,14 +7,18 @@ class Minesweeper extends StatefulWidget {
 }
 
 class MinesweeperState extends State<Minesweeper> {
-  final numOfHorizontalTiles = 12;
-  final numOvVerticalTiles = 20;
+  final numOfRows = 20;
+  final numOfColumns = 12;
+  final numOfMines = 40;
 
   bool gameStarted = false;
   List<List<String>> matrix = [];
+  List<List<String>> matrixsolucio =
+      []; // B = bomba, E = empty, num = num bombes adjecents
 
   @override
   Widget build(BuildContext context) {
+    setUpMatrix();
     return Scaffold(
         appBar: AppBar(title: const Text("Minesweeper")),
         body: InteractiveViewer(
@@ -22,26 +27,25 @@ class MinesweeperState extends State<Minesweeper> {
               const Expanded(
                 child: Divider(color: Colors.transparent),
               ),
-              rowBuild(),
-              rowBuild(),
-              rowBuild(),
-              rowBuild(),
-              rowBuild(), //5
-              rowBuild(),
-              rowBuild(),
-              rowBuild(),
-              rowBuild(),
-              rowBuild(), //10
-              rowBuild(),
-              rowBuild(),
-              rowBuild(),
-              rowBuild(),
-              rowBuild(), //15
-              rowBuild(),
-              rowBuild(),
-              rowBuild(),
-              rowBuild(),
-              rowBuild(), //20
+              rowBuild(0),
+              rowBuild(1),
+              rowBuild(2),
+              rowBuild(3),
+              rowBuild(4),
+              rowBuild(5),
+              rowBuild(6),
+              rowBuild(7),
+              rowBuild(8),
+              rowBuild(9),
+              rowBuild(11),
+              rowBuild(12),
+              rowBuild(13),
+              rowBuild(14),
+              rowBuild(15),
+              rowBuild(16),
+              rowBuild(17),
+              rowBuild(18),
+              rowBuild(19),
               const Expanded(
                 child: Divider(color: Colors.transparent),
               ),
@@ -50,52 +54,63 @@ class MinesweeperState extends State<Minesweeper> {
         ));
   }
 
-  Widget minesButton() {
+  Widget minesButton(int x, int y) {
+    // aquí guardarem la id de cada botó
+    int buttonX = x, buttonY = y;
     return Container(
-      width: MediaQuery.of(context).size.width / (numOfHorizontalTiles - 1),
-      height: MediaQuery.of(context).size.width / (numOfHorizontalTiles - 1),
+      // mides
+      width: MediaQuery.of(context).size.width / (numOfColumns + 2),
+      height: MediaQuery.of(context).size.width / (numOfColumns + 2),
+      // decoració i estils
       decoration: BoxDecoration(
           color: Colors.blue,
           border: Border.all(
             color: Colors.blue,
-            width: MediaQuery.of(context).size.width * 0.002,
+            width: MediaQuery.of(context).size.width * 0.001,
           )),
+      // fill button
       child: TextButton(
+        // estil
         style: TextButton.styleFrom(
           minimumSize: Size.square(
-              MediaQuery.of(context).size.width / (numOfHorizontalTiles - 1)),
+              MediaQuery.of(context).size.width / (numOfColumns + 2)),
         ),
-        onPressed: null,
+        // funció al premer
+        onPressed: () {
+          setState(() {
+            if (!gameStarted) {
+              startGame(x, y);
+              gameStarted = true;
+            }
+          });
+        },
+        // fill
         child: Text(
-          "a",
-          style: TextStyle(
-            fontSize: 10,
-            color: Colors.white,
-          ),
-          textAlign: TextAlign.center,
+          matrixsolucio[x][y],
+          style: TextStyle(fontSize: 12, color: Colors.white),
         ),
       ),
     );
   }
 
-  Widget rowBuild() {
+  Widget rowBuild(int row) {
     return Row(
       children: [
         const Expanded(
           child: Divider(color: Colors.transparent),
         ),
-        minesButton(),
-        minesButton(),
-        minesButton(),
-        minesButton(),
-        minesButton(), //5
-        minesButton(),
-        minesButton(),
-        minesButton(),
-        minesButton(),
-        minesButton(), // 10
-        minesButton(),
-        minesButton(), // 12
+        minesButton(row, 0),
+        minesButton(row, 1),
+        minesButton(row, 2),
+        minesButton(row, 3),
+        minesButton(row, 4),
+        minesButton(row, 5),
+        minesButton(row, 6),
+        minesButton(row, 7),
+        minesButton(row, 8),
+        minesButton(row, 9),
+        minesButton(row, 10),
+        minesButton(row, 11),
         const Expanded(
           child: Divider(color: Colors.transparent),
         ),
@@ -103,5 +118,110 @@ class MinesweeperState extends State<Minesweeper> {
     );
   }
 
-  void startGame() {}
+  void setUpMatrix() {
+    for (var i = 0; i < numOfRows; i++) {
+      List<String> row = [];
+      for (var j = 0; j < numOfColumns; j++) {
+        row.add("");
+      }
+      matrix.add(row);
+      matrixsolucio.add(row);
+    }
+  }
+
+  void startGame(int xo, int yo) {
+    // posem les bombes dins la matriu solució "millorar funció fent-la while o algo"
+    for (var i = 0; i < numOfMines; i++) {
+      var bombX = Random().nextInt(numOfRows);
+      var bombY = Random().nextInt(numOfColumns);
+
+      if (matrixsolucio[bombX][bombY] == "" &&
+          !(bombX >= xo - 1 &&
+              bombX <= xo + 1 &&
+              bombY >= yo - 1 &&
+              bombY <= yo + 1)) {
+        matrixsolucio[bombX][bombY] = "B";
+      } else {
+        i--;
+      }
+    }
+
+    // asignem els numeros corresponents als veins de les bombes i els empties
+    for (var i = 0; i < numOfRows; i++) {
+      for (var j = 0; j < numOfColumns; j++) {
+        if (matrixsolucio[i][j] != "B") {
+          var numVeinsBomba = veinsBomba(i, j);
+          if (numVeinsBomba != 0) {
+            matrixsolucio[i][j] = numVeinsBomba.toString();
+          } else {
+            matrixsolucio[i][j] = "";
+          }
+        }
+      }
+    }
+  }
+
+  int veinsBomba(int x, int y) {
+    var numVeinsBomba = 0;
+
+    // N
+    if (x != 0) {
+      // NW
+      if (y != 0) {
+        if (matrixsolucio[x - 1][y - 1] == "B") {
+          numVeinsBomba++;
+        }
+      }
+
+      // NE
+      if (y != numOfColumns - 1) {
+        if (matrixsolucio[x - 1][y + 1] == "B") {
+          numVeinsBomba++;
+        }
+      }
+
+      // N
+      if (matrixsolucio[x - 1][y] == "B") {
+        numVeinsBomba++;
+      }
+    }
+
+    // W
+    if (y != 0) {
+      if (matrixsolucio[x][y - 1] == "B") {
+        numVeinsBomba++;
+      }
+    }
+
+    // E
+    if (y != numOfColumns - 1) {
+      if (matrixsolucio[x][y + 1] == "B") {
+        numVeinsBomba++;
+      }
+    }
+
+    // S
+    if (x != numOfRows - 1) {
+      // SW
+      if (y != 0) {
+        if (matrixsolucio[x + 1][y - 1] == "B") {
+          numVeinsBomba++;
+        }
+      }
+
+      // SE
+      if (y != numOfColumns - 1) {
+        if (matrixsolucio[x + 1][y + 1] == "B") {
+          numVeinsBomba++;
+        }
+      }
+
+      // S
+      if (matrixsolucio[x + 1][y] == "B") {
+        numVeinsBomba++;
+      }
+    }
+
+    return numVeinsBomba;
+  }
 }
