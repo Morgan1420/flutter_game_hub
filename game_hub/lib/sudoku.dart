246bba7b95707d8980ca3ef3acb1aeb002ed30e8
Morgan1420 <jan.moran.r@gmail.com>
2023-03-05 19:48:19 +0100

    sudoku branch created

---
+++ game_hub/lib/sudoku.dart
@@ -0,0 +1,13 @@
+import 'dart:math';
+import 'package:flutter/material.dart';
+
+class Sudoku extends StatefulWidget {
+  @override
+  SudokuState createState() => SudokuState();
+}
+
+class SudokuState extends State<Sudoku> {
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
